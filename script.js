e19a3a2ae1f2fd94f4d21aaba9bd3a5a52f410da
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-15 21:39:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const tags = document.querySelectorAll('li');
+let i = -1;
+
+tags.forEach((tag, index) => {
+  if (tag.getAttribute('id') === 'level') {
+    i = index;
+	alert(`The level of the element is: ${i+1}`)
+  }
+	
+});
+
